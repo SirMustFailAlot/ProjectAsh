032a6aede45eef95644feb531e1ad429104fe456
Kayton <65788541+KaytonRex@users.noreply.github.com>
2025-03-14 22:09:08 +0000

    Initial Creation
    Cobblemon Spawn Notification Mod Creation.
    
    Looks for Shiny, Legendary, and then will send a message to a discord channel through a discord webhook.
    
    There are plans to improve this in the future.

---
+++ fabric-1.20.1/src/main/kotlin/io/github/sirmustfailalot/ProjectAsh.kt
@@ -0,0 +1,24 @@
+package io.github.sirmustfailalot
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+// Cobblemon
+import com.cobblemon.mod.common.api.Priority
+import com.cobblemon.mod.common.api.events.CobblemonEvents
+
+// Spawn Handler
+import io.github.sirmustfailalot.spawnHandler.SpawnHandler
+import com.google.gson.JsonParser
+import java.io.File
+
+object ProjectAsh : ModInitializer {
+    private val logger = LoggerFactory.getLogger("project-ash")
+	val discordWebhookURL = ""
+	override fun onInitialize() {
+
+
+		CobblemonEvents.POKEMON_ENTITY_SPAWN.subscribe(Priority.LOWEST, SpawnHandler::handle)
+		logger.info("Project Ash ----------- Oh Dang, Did we Load, Heck Yeah!")
+	}
+}
---
+++ fabric-1.20.1/src/main/kotlin/io/github/sirmustfailalot/spawnHandler/SpawnHandler.kt
@@ -0,0 +1,85 @@
+package io.github.sirmustfailalot.spawnHandler
+
+import org.slf4j.LoggerFactory
+
+import com.cobblemon.mod.common.api.events.entity.SpawnEvent
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.cobblemon.mod.common.util.toVec3d
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.extensions.jsonBody
+import com.github.kittinunf.result.Result
+import com.google.gson.JsonParser
+import kotlinx.coroutines.runBlocking
+
+import io.github.sirmustfailalot.ProjectAsh
+import java.io.File
+
+object SpawnHandler {
+    private val logger = LoggerFactory.getLogger("project-ash")
+
+    fun handle(spawn: SpawnEvent<PokemonEntity>) {
+
+
+        // Get Basic Information e.g. world, position, Pokémon
+        val world = spawn.ctx.world
+        val dimensionName = world.dimension().toString()
+        val dimension = when {
+            world.dimension().toString().contains("overworld") -> "Overworld"
+            world.dimension().toString().contains("the_nether") -> "Nether"
+            world.dimension().toString().contains("the_end") -> "End"
+            else -> return
+        }
+
+        // Get Position
+        val pos = spawn.ctx.position
+        val posValue = pos.x.toString() + ", " + pos.y.toString() + ", " + pos.z.toString()
+
+        // Get Nearest Player
+        val players = spawn.ctx.world.players()
+            .filter {pos.distToCenterSqr(pos.toVec3d()) <= 256 * 256 }
+            .minByOrNull {pos.distToCenterSqr(pos.toVec3d())}
+
+        // Check to see if it's a shiny or a label that we like
+        val pokemon = spawn.entity.pokemon
+        val shiny = pokemon.shiny
+        val labelsWeWant = listOf("legendary")
+        val label = pokemon.form.labels.firstOrNull { it in labelsWeWant}
+
+        val spawntype = when {
+            shiny && label != null -> "Shiny Legendary"
+            shiny && label == null -> "Shiny"
+            !shiny && label != null -> "Legendary"
+            else -> return // Fallback case
+        }
+
+        val species = pokemon.species.translatedName.string
+
+        val message = players?.name?.string + "! " + spawntype + " " + species + " " + dimension + " at " + posValue
+        logger.info(message)
+
+        var discordWebhookURL = ""
+        val jsonContent = File("config/ProjectAsh.json").readText()
+        val jsonObject = JsonParser.parseString(jsonContent).asJsonObject
+        discordWebhookURL = jsonObject.get("discordWebhookURL").asString
+
+        val jsonBody = """
+            {
+                "content": "$message"
+            }
+        """.trimIndent()
+
+        Fuel.post(discordWebhookURL)
+            .jsonBody(jsonBody)
+            .response { _, response, result ->
+                when (result) {
+                    is Result.Success -> {
+                        println("Message sent successfully! Status: ${response.statusCode}")
+                    }
+                    is Result.Failure -> {
+                        val error = result.getException()
+                        println("Error sending message: ${error.message}")
+                    }
+                }
+            }
+    }
+}
