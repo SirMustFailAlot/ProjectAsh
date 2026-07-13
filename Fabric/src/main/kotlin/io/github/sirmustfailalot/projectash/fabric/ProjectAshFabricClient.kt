49ab9b15c2bd6424f10c3c113054b123dffdf1ea
Rex <65788541+SirMustFailAlot@users.noreply.github.com>
2026-07-13 13:43:34 +0100

    Hotkey Description / Mod Icon and Desc
    Changed the Hotkey to ensure that this shows "Project Ash" as the category, and then "Showcase" for the keybinding.
    
    Added proper descriptions and details for the mod, along with an icon.

--- Fabric/src/main/kotlin/io/github/sirmustfailalot/projectash/fabric/ProjectAshFabricClient.kt
+++ Fabric/src/main/kotlin/io/github/sirmustfailalot/projectash/fabric/ProjectAshFabricClient.kt
@@ -1,15 +1,18 @@
 package io.github.sirmustfailalot.projectash.fabric
 
+import com.mojang.blaze3d.platform.InputConstants
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.KeyMapping
 import org.lwjgl.glfw.GLFW
 
 object ProjectAshFabricClient : ClientModInitializer {
+
     val showcaseKeyBind: KeyMapping = KeyMapping(
         "key.projectash.showcase",
+        InputConstants.Type.KEYSYM,
         GLFW.GLFW_KEY_H,
-        "category.projectash"
+        "key.categories.projectash"
     )
 
     override fun onInitializeClient() {
